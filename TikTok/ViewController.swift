c36ee7aa8be9fd9d9e1112d9e5dff7f3e64c2ee0
Kxy <823942471@qq.com>
2020-06-13 19:14:25 +0800

    Initial Commit

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Kxy on 2020/6/13.
+//  Copyright © 2020 Kxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
